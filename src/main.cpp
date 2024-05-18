44b5e5c1c72132aedb5ff143e6dbcd89b7e30d04
Mark <sotaogata777@gmail.com>
2024-05-18 17:05:37 -0400

    line, compass, orbit, dampen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <zirconLib.h>
 
-#define LINE_THRESHOLD 650
-#define MOTOR_POWER 100
+#define LINE_THRESHOLD 450
+#define MOTOR_POWER 140
+#define BALL_CALIBRATION_X 0
+#define BALL_CALIBRATION_Y 0
 
 // correct ball vector
 // compass angle readings
@@ -65,7 +67,7 @@ Vector2D readBallSensors() {
 
 void moveMotors(const Vector2D& direction, float rotationFactor) {
     float angle = atan2(direction.y, direction.x);
-    Serial.println("angle " + String(180*angle/M_PI));
+    // Serial.println("angle " + String(180*angle/M_PI));
     
 
     // Calculate motor powers (assuming 3 omni-directional wheels)
@@ -93,18 +95,29 @@ void setup(void)
   Serial.begin(115200);
   InitializeZircon();
   motor1(50, 0);
-  delay(200);
+  delay(500);
   motor1(0,0);
   while (readButton(1) == 0) {
     goalAngle = readCompass();
   }
 }
 
+float move_angle_from_default;
+Vector2D moveVector;
+Vector2D ballVector;
 
 void loop(void)
 {
 
-  Vector2D ballVector = readBallSensors();
+  // read ball sensors
+  ballVector = readBallSensors();
+  Serial.println("ball vector: " + ballVector.toString());
+
+  // adjust ball detection for calibration
+  ballVector.x += BALL_CALIBRATION_X;
+  ballVector.y += BALL_CALIBRATION_Y;
+
+
 
   int compassDiff = readCompass() - goalAngle;
   if (compassDiff > 180) {
@@ -113,56 +126,60 @@ void loop(void)
     compassDiff += 360;
   }
 
-  // Serial.println("line sensors: " + String(readLine(1)) + " " + String(readLine(2)) + " " + String(readLine(3)));
-  // Serial.println("compass diff: " + String(compassDiff) + " ball vector: " + ballVector.toString());
 
   if (readLine(1) > LINE_THRESHOLD) {
-    motor1(100, 0);
-    motor2(100, 1);
+    motor1(MOTOR_POWER, 0);
+    motor2(MOTOR_POWER, 1);
     motor3(0, 0);
   } else if (readLine(2) > LINE_THRESHOLD) {
     motor1(0, 0);
-    motor2(100, 0);
-    motor3(100, 1);
+    motor2(MOTOR_POWER, 0);
+    motor3(MOTOR_POWER, 1);
   } else if (readLine(3) > LINE_THRESHOLD) {
-    motor1(100, 1);
+    motor1(MOTOR_POWER, 1);
     motor2(0, 0);
-    motor3(100, 0);
+    motor3(MOTOR_POWER, 0);
 
 
-  // } else if (abs(compassDiff) > 10) {
-  //   int speed = abs(compassDiff) + 40;
-  //   int direction = (compassDiff < 0) ? 0 : 1;
-  //   motor1(speed, direction);
-  //   motor2(speed, direction);
-  //   motor3(speed, direction);
+
+
+
     
   } else {
-    Serial.println("ball vector: " + ballVector.toString() + " compass diff: " + String(compassDiff/180.0));
-    moveMotors(ballVector, compassDiff/180.0);
+    // Serial.println("ball vector: " + ballVector.toString() + " compass diff: " + String(compassDiff/180.0));
+
+    // orbit
+    
+    float angle = atan2(ballVector.y, ballVector.x);
+    if (angle > M_PI) {
+      angle -= M_PI;
+    } else if (angle < -M_PI) {
+      angle += M_PI;
+    }
+    float angle_from_front = angle - M_PI/2;
+    float orbit_addition = min(M_PI/2, M_PI*(0.04f * pow(M_E, 0.12f * abs(180*angle_from_front/M_PI)))/180);
+    
+    float ball_vec_magnitude = sqrt(ballVector.x * ballVector.x + ballVector.y * ballVector.y);
+    float dampened_orbit_addition = orbit_addition * max(1.0, 0.04f * pow(M_E, 0.004f * (ball_vec_magnitude - 100)));
+    
+    if (angle_from_front < 0) {
+      dampened_orbit_addition *= -1;
+    }
+
+    move_angle_from_default = angle_from_front + dampened_orbit_addition + M_PI/2;
+    
+    moveVector.x = cos(move_angle_from_default);
+    moveVector.y = sin(move_angle_from_default);
+
+
+    if (ball_vec_magnitude < 30) {
+      moveVector.x = 0;
+      moveVector.y = 0;
+    }
+    moveMotors(moveVector, compassDiff/180.0);
   }
 
 
-  
-  // if (readBall(1) > 650) {
-
-  //   if (abs(goalAngle - readCompass()) < 30) {
-  //     motor1(100, 0);
-  //     motor2(100, 1);
-  //     motor3(0, 0);
-  //   } else {
-  //     motor1(70, 0);
-  //     motor2(10, 1);
-  //     motor3(70, 1);
-  //   }
-    
-  // } else {
-  //   motor1(40, 1);
-  //   motor2(40, 1);
-  //   motor3(40, 1);
-  // }
-
-  delay(100);
 
 
 }
